b8e8b121e5e0b00723431b3fabdc07e872f3c488
Pauline Gatei <gateipaulinem@gmail.com>
2022-09-15 16:42:44 +0800

    Setting up the app

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def main_page():
+	st.markdown("# Main page 🔎")
+	st.sidebar.success("About")
+	st.write("""Get ready to be marveled""")
+
+if __name__ == '__main__':
+	main_page()
+
+
+
