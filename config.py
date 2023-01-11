85c1eb521de474e9b94c0cbbfbeea07195d567b1
Pauline Gatei <gateipaulinem@gmail.com>
2023-01-11 18:17:13 +0800

    Deployment update

--- config.py
+++ config.py
@@ -15,19 +15,19 @@ class Config(object):
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
-class ProductionConfig(Config):
-    ENV='production'
-    SECRET_KEY = os.environ['SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','').replace('postgres://', 'postgresql://') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+# class ProductionConfig(Config):
+#     ENV='production'
+#     SECRET_KEY = os.environ['SECRET_KEY']
+#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','').replace('postgres://', 'postgresql://') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 
-class DevelopmentConfig(Config):
-    FLASK_ENV='development'
-    DEBUG=True
+# class DevelopmentConfig(Config):
+#     FLASK_ENV='development'
+#     DEBUG=True
 
-class TestingConfig(Config):
-  ENV='testing'
-  SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir,'tests/test.db')
-  #SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:' #in memory database
+# class TestingConfig(Config):
+#   ENV='testing'
+#   SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir,'tests/test.db')
+#   #SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:' #in memory database
   
 
 
